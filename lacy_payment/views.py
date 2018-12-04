5844161135b0a49866d7ab0b9ca127fb1e389ed9
samaritainsims <sfaysamaritan@gmail.com>
2018-12-04 12:31:09 +0100

    Second

--- lacy_payment/views.py
+++ lacy_payment/views.py
@@ -2,6 +2,7 @@ import json
 import urllib
 import urllib.request as urllib2
 
+import requests
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from pip.utils import logging
@@ -29,16 +30,35 @@ def payViaMomo(request):
         #sres = json.dump(res_json, 2)
         #return render(request, 'lacy_payment/payment_accepted.html', sres);
 
-    jdata = {"amount": 500, "currency":"XAF", "item_ref":"Item_bought",
+
+
+    """
+        jdata = {"amount": 500, "currency":"XAF", "item_ref":"Item_bought",
              "payment_ref":"ref_payment_in_our_server", "first_name":"Samaritain", "last_name":"Sims",
              "return_url":"http://127.0.0.1/lacy/momopaymentsuccess",
              "notify_url":"http://127.0.0.1/lacy/feedbackmomo"}
+        urlbase = "https://api.monetbil.com/widget/v2.1/Je0J3wOesnqdkuRx4lIpBYipc1mkObGH"
 
-    data = urllib.parse.urlencode(jdata).encode("utf-8");
-    headers = {'Content-Type': 'application/json'}
-    req = urllib2.Request("https://api.monetbil.com/widget/2.1/Je0J3wOesnqdkuRx4lIpBYipc1mkObGH", data=data, headers=headers);
-    res = urllib2.urlopen(req)
-    res_json = json.loads(res.read());
+        data = urllib.parse.urlencode(jdata)#.encode("utf-8");
+        print(urlbase + data);
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        req = urllib2.Request(urlbase, jdata, headers=headers);
+        res = urllib2.urlopen(req)
+        res_json = json.loads(res.read());
+    """
+
+    url = "https://api.monetbil.com/widget/v2.1/Je0J3wOesnqdkuRx4lIpBYipc1mkObGH"
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = {"amount": "500", "currency": "XAF",
+            "item_ref": "Item_bought", "locale": "fr", "country": "CM",
+            "payment_ref": "ref_payment_in_our_server", "first_name": "Samaritain", "last_name": "Sims", "phone" : "697266488", "email" : "s@s.s",
+            "return_url": "http://127.0.0.1/lacy/momopaymentsuccess",
+            "notify_url": "http://127.0.0.1/lacy/feedbackmomo"}
+    res = requests.post(url, headers=headers, data=urllib.parse.urlencode(data))
+    res_json = res.json()
+    print(res_json)
 
     if (res_json is None):
         return HttpResponse("""
